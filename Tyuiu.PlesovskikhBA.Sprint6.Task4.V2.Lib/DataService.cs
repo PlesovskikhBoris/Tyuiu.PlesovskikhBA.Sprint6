0ad8700c0f457ce775a9b14612785a1a9394e1e4
Administrator <Administrator@WIN-2ORNOHFEDP4>
2025-12-15 04:22:11 +0500

    Task4

---
+++ Tyuiu.PlesovskikhBA.Sprint6.Task4.V2.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.PlesovskikhBA.Sprint6.Task4.V2.Lib
+{
+    public class DataService : ISprint6Task4V2
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] valueArray;
+            int len = (stopValue - startValue) + 1;
+            valueArray = new double[len];
+            double F;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                F = Math.Round((Math.Cos(x) / (x - 0.7) - Math.Sin(x) * (12 * (x))) + 2, 2);
+                valueArray[count] = F;
+                count++;
+
+            }
+            return valueArray;
+        }
+    }
+}
---
+++ Tyuiu.PlesovskikhBA.Sprint6.Task4.V2/FormMain.cs
@@ -0,0 +1,75 @@
+﻿using Tyuiu.PlesovskikhBA.Sprint6.Task4.V2.Lib;
+using System.Windows.Forms.VisualStyles;
+namespace Tyuiu.PlesovskikhBA.Sprint6.Task4.V2
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        DataService ds = new DataService();
+
+        private void buttonResult_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int start = Convert.ToInt32(textBoxStart.Text);
+                int stop = Convert.ToInt32(textBoxEnd.Text);
+
+                int len = ds.GetMassFunction(start, stop).Length;
+
+                double[] valueArray;
+                valueArray = new double[len];
+
+                valueArray = ds.GetMassFunction(start, stop);
+
+
+                this.chartResult.ChartAreas[0].AxisX.Title = "Ось X";
+                this.chartResult.ChartAreas[0].AxisY.Title = "Ось Y";
+
+                textBoxResult.Text = "";
+
+                chartResult.Series[0].Points.Clear();
+                for (int i = 0; i < len - 1; i++)
+                {
+                    this.chartResult.Series[0].Points.AddXY(start, valueArray[i]);
+                    textBoxResult.AppendText(valueArray[i] + Environment.NewLine);
+                    start++;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Введены неверные данные", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonInfo_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Таск 4 выполнил студент группы СМАРТб-24-1 Плесовских Б. А.", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = $@"{Directory.GetCurrentDirectory()}\OutPutFileTask4.txt";
+                File.WriteAllText(path, textBoxResult.Text);
+
+                DialogResult dialogResult = MessageBox.Show("Файл" + path + "сохранен успешно!\n Открыть его в блокноте?", "Сообщение", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+
+                if (dialogResult == DialogResult.Yes)
+                {
+                    System.Diagnostics.Process txt = new System.Diagnostics.Process();
+                    txt.StartInfo.FileName = "notepad.exe";
+                    txt.StartInfo.Arguments = path;
+                    txt.Start();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Сбой при сохранении файла", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
