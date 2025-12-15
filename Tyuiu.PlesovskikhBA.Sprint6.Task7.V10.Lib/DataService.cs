eb133270250c7d2972eb91ed2460413f3b61c815
Administrator <Administrator@WIN-2ORNOHFEDP4>
2025-12-15 06:20:26 +0500

    Task7

--- Tyuiu.PlesovskikhBA.Sprint6.Task7.V10.Lib/DataService.cs
+++ Tyuiu.PlesovskikhBA.Sprint6.Task7.V10.Lib/DataService.cs
@@ -3,29 +3,38 @@ namespace Tyuiu.PlesovskikhBA.Sprint6.Task7.V10.Lib
 {
     public class DataService : ISprint6Task7V10
     {
-
         public int[,] GetMatrix(string path)
         {
-            string[] file = File.ReadAllText(path).Split('\n');
-            string[] cs = file[4].Split(';');
-            int[,] m = new int[file.Length, file[0].Split(';').Length];
-            for (int i = 0; i < file[0].Split(';').Length; i++)
+            string[] lines = File.ReadAllText(path).Replace('\n', '\r').Split('\r', StringSplitOptions.RemoveEmptyEntries);
+
+            int rows = lines.Length;
+
+
+            int cols = lines[0].Split(';').Length;
+            int[,] matrix = new int[rows, cols];
+
+
+            for (int i = 0; i < rows; i++)
             {
-                string a = file[4].Split(';')[i];
-                string[] b = file[4].Split(';');
-                int c = int.Parse(a);
-                if (int.Parse(cs[i]) <= 10 && int.Parse(cs[i]) >= 5) { cs[i] = "0"; }
-                ;
-            }
-            file[4] = string.Join(';', cs);
-            for (int i = 0; i < file[0].Split(';').Length; i++)
-            {
-                for (int j = 0; j < file.Length; j++)
+                string[] values = lines[i].Split(';');
+                for (int j = 0; j < cols; j++)
                 {
-                    m[i, j] = int.Parse(file[i].Split(';')[j]);
+                    matrix[i, j] = Convert.ToInt32(values[j]);
                 }
             }
-            return m;
+
+            if (rows > 4)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (matrix[4, j] >= 5 && matrix[4, j] < 10)
+                    {
+                        matrix[4, j] = 0;
+                    }
+                }
+            }
+
+            return matrix;
         }
     }
 }
