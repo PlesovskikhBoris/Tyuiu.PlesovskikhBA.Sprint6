998080f286f5285602fba7cbc1631284c0a37316
Administrator <Administrator@WIN-2ORNOHFEDP4>
2025-12-15 06:25:50 +0500

    Task7

--- Tyuiu.PlesovskikhBA.Sprint6.Task7.V10.Lib/DataService.cs
+++ Tyuiu.PlesovskikhBA.Sprint6.Task7.V10.Lib/DataService.cs
@@ -8,11 +8,8 @@ namespace Tyuiu.PlesovskikhBA.Sprint6.Task7.V10.Lib
             string[] lines = File.ReadAllText(path).Replace('\n', '\r').Split('\r', StringSplitOptions.RemoveEmptyEntries);
 
             int rows = lines.Length;
-
-
             int cols = lines[0].Split(';').Length;
             int[,] matrix = new int[rows, cols];
-
 
             for (int i = 0; i < rows; i++)
             {
@@ -27,7 +24,7 @@ namespace Tyuiu.PlesovskikhBA.Sprint6.Task7.V10.Lib
             {
                 for (int j = 0; j < cols; j++)
                 {
-                    if (matrix[4, j] >= 5 && matrix[4, j] < 10)
+                    if (matrix[4, j] >= 5 && matrix[4, j] <= 10)
                     {
                         matrix[4, j] = 0;
                     }
